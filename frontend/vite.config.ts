ac8a2b146240338ed487e0495d929caded4c8305
Touko Toivonen <touko.t.toivonen@gmail.com>
2026-01-19 13:55:48 +0200

    fix: now works

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi"],
+  },
 })
